30205e2ba29d521d3aaedc2cdd7b7d7de9eaad18
Maengdok <pionaxel@gmail.com>
2023-01-18 12:52:06 +0100

    feat: history api, lauches api and capsule api

--- src/components/Capsules.js
+++ src/components/Capsules.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 import {useEffect, useState} from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+
 
 export const Capsules = () => {
     const [data, setData] = useState([]);
@@ -16,19 +25,38 @@ export const Capsules = () => {
 
     return(
         <div className={"capsules"}>
-            <h2>Capsules</h2>
-            <div className={"capsules-response"}>
-                <div>
-                    {
-                        data.map(capsule => (
-                            <div key={capsule.id}>
-                                <h3>{capsule.serial}</h3>
-                                <p>Launch date: {capsule.original_launch}</p>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
+            <Button variant={"contained"} href={"/"}>Return</Button>
+            <h2>Launches</h2>
+            <TableContainer component={Paper}>
+                <Table aria-label={"Capsules"}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Serial</TableCell>
+                            <TableCell>Type</TableCell>
+                            <TableCell>Status</TableCell>
+                            <TableCell>Last Update</TableCell>
+                            <TableCell>Reuse count</TableCell>
+                            <TableCell>Water landings</TableCell>
+                            <TableCell>Land Landings</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            data.map(capsule => (
+                                <TableRow key={capsule.id}>
+                                    <TableCell><h3>{capsule.serial}</h3></TableCell>
+                                    <TableCell><p>{capsule.type}</p></TableCell>
+                                    <TableCell><p>{capsule.status}</p></TableCell>
+                                    <TableCell><p>{capsule.last_update}</p></TableCell>
+                                    <TableCell><p>{capsule.reuse_count}</p></TableCell>
+                                    <TableCell><p>{capsule.water_landings}</p></TableCell>
+                                    <TableCell><p>{capsule.land_landings}</p></TableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
 };
--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,5 +1,18 @@
 import axios from 'axios';
 import {useEffect, useState} from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import SvgIcon from '@mui/material/SvgIcon';
+import SearchIcon from '@mui/icons-material/Search';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+
 
 export const Launches = () => {
     const [data, setData] = useState([]);
@@ -16,18 +29,53 @@ export const Launches = () => {
 
     return(
         <div className={"launches"}>
+            <Button variant={"contained"} href={"/"}>Return</Button>
             <h2>Launches</h2>
-            <div className={"launches-response"}>
-                <div>
-                    {
-                        data.map(launch => (
-                            <div key={launch.id}>
-                                <h3>{launch.name}</h3>
-                                <p>Launch date: {launch.date_local}</p>
-                            </div>
-                        ))
-                    }
-                </div>
+            <TableContainer component={Paper}>
+                <Table aria-label={"Launches"}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Patch</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Rocket</TableCell>
+                            <TableCell>Launch Date UTC</TableCell>
+                            <TableCell>Success</TableCell>
+                            <TableCell>Article</TableCell>
+                            <TableCell>Video</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            data.map(launch => (
+                                <TableRow key={launch.id}>
+                                    <TableCell><img className={"patch-image"} src={launch.links.patch.small} alt={"launch patch picture"} /></TableCell>
+                                    <TableCell><h3>{launch.name}</h3></TableCell>
+                                    <TableCell>{launch.rocket}</TableCell>
+                                    <TableCell><p>{launch.date_local}</p></TableCell>
+                                    <TableCell><p>{launch.success ? 'Yes' : 'False'}</p></TableCell>
+                                    <TableCell>
+                                        <Link href={launch.links.article}>
+                                            <Button variant={"contained"}>
+                                                <SvgIcon component={SearchIcon} inheritViewBox />
+                                            </Button>   
+                                        </Link>
+                                    </TableCell>
+                                    <TableCell>
+                                        <Link href={'https://youtube.com/watch?v=' + launch.links.youtube_id}>
+                                            <Button variant={"contained"} sx={{background: '#ff0000'}}>
+                                                <SvgIcon component={PlayArrowIcon} inheritViewBox />
+                                            </Button>
+                                        </Link>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <div>
+
             </div>
         </div>
     );
